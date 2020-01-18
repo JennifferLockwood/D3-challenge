e3d38c5dc577e6b21423ea9d9ff43c659e8892a9
Jenniffer Lockwood <jennifferlockwood@gmail.com>
2020-01-18 18:45:39 -0500

    Final Commit.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
-// @TODO: YOUR CODE HERE!
+// D3.js challenge
 var svgWidth = 960;
-var svgHeight = 500;
+var svgHeight = 560;
 
 var margin = {
   top: 20,
   right: 40,
   bottom: 60,
-  left: 100
+  left: 60
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -34,11 +34,11 @@ d3.csv("assets/data/data.csv").then(function(risksData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([8, d3.max(risksData, d => d.poverty)])
+      .domain([8, d3.max(risksData, d => d.poverty+2)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(risksData, d => d.healthcare)])
+      .domain([4, d3.max(risksData, d => d.healthcare+2)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -73,49 +73,30 @@ d3.csv("assets/data/data.csv").then(function(risksData) {
     .append("text")
     .attr("x", d => xLinearScale(d.poverty))
     .attr("y", d => yLinearScale(d.healthcare))
+    .attr("dy", ".35em")
     .text(d => d.abbr)
     .attr("text-anchor", "middle")
     .attr("font-family", "sans-serif")
     .attr("font-size", "10px")
-    .attr("fill", "white");
-
-
-    // Step 6: Initialize tool tip
-    // ==============================
-    // var toolTip = d3.tip()
-    //   .attr("class", "tooltip")
-    //   .offset([80, -60])
-    //   .html(function(d) {
-    //     return (`${d.state}<br>Hair length: ${d.poverty}<br>Hits: ${d.healthcare}`);
-    //   });
-
-    // Step 7: Create tooltip in the chart
-    // ==============================
-    // chartGroup.call(toolTip);
-
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
-    // circlesGroup.on("click", function(data) {
-    //   toolTip.show(data, this);
-    // })
-    //   // onmouseout event
-    //   .on("mouseout", function(data, index) {
-    //     toolTip.hide(data);
-    //   });
+    .attr("fill", "white")
+    .attr("font-weight", "700");
 
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
+      .attr("y", 0 - margin.left + 10)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Lacks Healthcare (%)");
+      .text("Lacks Healthcare (%)")
+      .attr("text-anchor", "middle")
+      .attr("font-weight", "700");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("In Poverty (%)");
+      .text("In Poverty (%)")
+      .attr("font-weight", "700");
   }).catch(function(error) {
     console.log(error);
   });
